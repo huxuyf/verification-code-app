f711ede9106ee2135329ce16fa8da4a5806e4a39
huxuyf <shingu@gmail.com>
2026-04-04 15:16:23 +0800

    添加 android.content.Intent 的导入

--- app/src/main/java/com/verificationcoder/app/VerifyCodeActivity.kt
+++ app/src/main/java/com/verificationcoder/app/VerifyCodeActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
